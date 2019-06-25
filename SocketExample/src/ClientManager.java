5fb44c09cb3384cfb483aa27f88b094426cb69ca
Amir Behmanesh <amirrezabsh79@gmail.com>
2019-06-26 01:57:55 +0430

    converting mp3 file to byte array added

--- SocketExample/src/ClientManager.java
+++ SocketExample/src/ClientManager.java
@@ -1,11 +1,14 @@
+import javax.swing.*;
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ClientManager implements Runnable{
     private Socket ServerSocket;
     private PrintWriter writer;
     private DataInputStream dataInputStream;
+    private ArrayList <String> sharedList = new ArrayList<>();
+    private String title;
     public ClientManager(Socket socket) throws IOException {
         ServerSocket = socket;
         dataInputStream = new DataInputStream(ServerSocket.getInputStream());
@@ -15,5 +18,27 @@ public class ClientManager implements Runnable{
     @Override
     public void run() {
 
+    }
+    public JFileChooser addMusic () throws IOException {
+        JFileChooser f=new JFileChooser();
+        f. showSaveDialog (null) ;
+        String path=f.getSelectedFile().getAbsolutePath ();
+        File song = new File(path);
+        FileInputStream file = new FileInputStream(path);
+        int size = (int)song.length();
+        file.skip(size - 128);
+        byte[] last128 = new byte[128];
+        file.read(last128);
+        String id3 = new String(last128);
+        this.title = id3.substring(3,32);
+        sharedList.add(title);
+        file.close();
+        return f;
+    }
+    public byte[] fileToByte (File file) throws IOException {
+        FileInputStream f = new FileInputStream(file);
+        byte [] fileToByte = new byte[(int) file.length()];
+        f.read(fileToByte);
+        return fileToByte;
     }
 }
